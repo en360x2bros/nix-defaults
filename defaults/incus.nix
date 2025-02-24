2ddf781e8306aa40011f37f8f48cb3d7653802db
Dennis Oderwald <oderwald@2bros-group.com>
2025-02-24 18:28:25 +0100

    incus default added

---
+++ defaults/incus.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, lib, ... }:
+
+{
+  config = {
+    virtualisation.incus.enable = true;
+    virtualisation.incus.package = pkgs.incus;
+    virtualisation.incus.ui.enable = lib.mkDefault false;
+  };
+}
--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
   outputs = { self, nixpkgs }: {
     nixosModules = {
       dns = import ./defaults/dns.nix;
+      incus = import ./defaults/incus.nix;
       locale = import ./defaults/locale.nix;
       mgmt = import ./defaults/mgmt.nix;
       packages = import ./defaults/packages.nix;
