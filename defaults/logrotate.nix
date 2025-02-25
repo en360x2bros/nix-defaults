e8dffc3e2641cde80797e2ac54221aa70aea51aa
Dennis Oderwald <oderwald@2bros-group.com>
2025-02-25 19:03:59 +0100

    logrotate in-line comments added

--- defaults/logrotate.nix
+++ defaults/logrotate.nix
@@ -11,13 +11,13 @@
       enable = true;
       settings = {
         "/var/log/**/*.log" = {
-          rotate = 7;
-          daily = true;
-          missingok = true;
-          compress = true;
-          delaycompress = true;
-          notifempty = true;
-          copytruncate = false;
+          rotate = 7; # Keep logs for 7 rotations (equivalent to 7 days with daily rotation)
+          daily = true; # Rotate logs daily
+          missingok = true; # Do not report an error if the log file is missing
+          compress = true; # Compress old log files to save disk space
+          delaycompress = true; # Do not compress the most recently rotated log file immediately
+          notifempty = true; # Do not rotate empty log files
+          copytruncate = false; # Do not truncate the log file in place; create a new one instead
         };
       };
     };
