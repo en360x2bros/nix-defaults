07775beb5d77a212913058e1daf0d7409f1c4cd8
Patrick Hütter <p.huetter@encircle360.com>
2025-02-25 14:46:18 +0100

    Changed default initrd ssh port to 2222

--- defaults/boot-initrd.nix
+++ defaults/boot-initrd.nix
@@ -4,6 +4,7 @@
   config = {
     boot.initrd.network.enable = true;
     boot.initrd.network.ssh.enable = true;
+    boot.initrd.network.ssh.port = 2222;
     boot.initrd.network.ssh.hostKeys = [
       "/etc/ssh/ssh_host_ed25519_key"
     ];
