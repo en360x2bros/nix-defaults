53eccc0cc10fbf86610d136e52447ef203c17545
Dennis Oderwald <oderwald@2bros-group.com>
2025-02-25 19:56:39 +0100

    nixfmt reformat

--- defaults/rclone.nix
+++ defaults/rclone.nix
@@ -1,7 +1,12 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
-	environment.systemPackages = with pkgs; [
-      rclone
-    ];
+  environment.systemPackages = with pkgs; [
+    rclone
+  ];
 }
