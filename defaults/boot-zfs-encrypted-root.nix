5fba69b83942cc332ae9e9ba447c895ba088a880
Patrick Hütter <p.huetter@encircle360.com>
2025-02-28 00:42:29 +0100

    fixed encapsulation

--- defaults/boot-zfs-encrypted-root.nix
+++ defaults/boot-zfs-encrypted-root.nix
@@ -1,14 +1,16 @@
+{
   boot = {
     kernelPackages = pkgs.linuxPackagesFor pkgs.linux_6_12;
     supportedFilesystems.zfs = true;
     zfs.package = pkgs.zfs_2_3;
     zfs.requestEncryptionCredentials = true;
     initrd.network = {
-        postCommands = ''
+      postCommands = ''
         # Import all pools
         zpool import -a
         # Add the load-key command to the .profile
         echo "zfs load-key -a; killall zfs" >> /root/.profile
-        '';
-      };
+      '';
+    };
   };
+}
