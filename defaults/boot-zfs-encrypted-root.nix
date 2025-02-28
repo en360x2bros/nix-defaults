e7d25fc39c8e4850d07d0bcee9b18cfab7bc227c
Dennis Oderwald <oderwald@2bros-group.com>
2025-02-28 12:46:41 +0100

    zfs-encrypted-root fixed

--- defaults/boot-zfs-encrypted-root.nix
+++ defaults/boot-zfs-encrypted-root.nix
@@ -10,11 +10,15 @@
     boot.supportedFilesystems.zfs = lib.mkDefault true;
     boot.zfs.package = lib.mkDefault pkgs.zfs_2_3;
     boot.zfs.requestEncryptionCredentials = lib.mkDefault true;
-    boot.initrd.network.postCommands = lib.mkDefault ''
+
+    boot.initrd.postDeviceCommands = lib.mkDefault ''
       # Import all pools
       zpool import -a
       # Add the load-key command to the .profile
-      echo "zfs load-key -a; killall zfs" >> /root/.profile
+      echo "#!/bin/sh" > /bin/askpass-zfs
+      echo "zfs load-key -a; killall zfs" >> /bin/askpass-zfs
+      chmod +x /bin/askpass-zfs
     '';
+    boot.initrd.network.ssh.shell = "/bin/askpass-zfs";
   };
 }
