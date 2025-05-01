377c7db9c2e24be58bf2d49d8e2730537c033cb0
Patrick Hütter <p.huetter@encircle360.com>
2025-05-01 16:35:55 +0200

    Set DNSSEC and DNS-over-TLS to false in resolved config
    Disable DNSSEC and DNS-over-TLS to address potential DNS issues. This ensures more reliable DNS resolution by preventing misconfigurations or compatibility problems.

--- defaults/dns.nix
+++ defaults/dns.nix
@@ -9,14 +9,14 @@
   config = {
     services.resolved = {
       enable = true;
-      dnssec = "true";
+      dnssec = "false"; # important to prevent dns issues
       domains = [ "~." ];
       fallbackDns = [
         "8.8.4.4#eight.eight.four.four"
         "1.0.0.1#one.zero.zero.one"
         "2001:4860:4860::8844#eight.eight.four.four"
       ];
-      dnsovertls = "true";
+      dnsovertls = "false"; # important to prevent dns issues
     };
 
     networking.nameservers = lib.mkDefault [
