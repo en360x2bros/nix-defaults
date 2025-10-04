fc5a90ba19cdd0f28abf5285b3485385098f1976
Dennis Oderwald <oderwald@2bros-group.com>
2025-10-04 16:18:18 +0200

    zfs-zed-notify added

---
+++ defaults/zfs-zed-notify.nix
@@ -0,0 +1,15 @@
+{ lib, pkgs, config, ... }:
+{
+  config = {
+    services.zfs.zed.settings = {
+      ZED_EMAIL_ADDR = [ "root" ];
+      # ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
+      ZED_EMAIL_OPTS = "@ADDRESS@";
+
+      ZED_NOTIFY_INTERVAL_SECS = 3600;
+      ZED_NOTIFY_VERBOSE = true;
+
+      ZED_SCRUB_AFTER_RESILVER = true;
+    };
+  };
+}
