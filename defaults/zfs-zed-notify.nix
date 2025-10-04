82cb6dbbc804b080b438b7f3bb41237444931aa4
Dennis Oderwald <oderwald@2bros-group.com>
2025-10-04 16:33:37 +0200

    zfs-zed-notify clean up

--- defaults/zfs-zed-notify.nix
+++ defaults/zfs-zed-notify.nix
@@ -3,7 +3,6 @@
   config = {
     services.zfs.zed.settings = {
       ZED_EMAIL_ADDR = [ "root" ];
-      # ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
       ZED_EMAIL_OPTS = "@ADDRESS@";
 
       ZED_NOTIFY_INTERVAL_SECS = 3600;
