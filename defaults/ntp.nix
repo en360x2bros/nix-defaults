4d09b0b68a9a00c582f6d4dd2f843d78edc6bb02
Patrick Hütter <p.huetter@encircle360.com>
2026-03-13 14:09:02 +0100

    Switch from NTP to Chrony and update time sync configuration

--- defaults/ntp.nix
+++ defaults/ntp.nix
@@ -7,9 +7,19 @@
 
 {
   config = {
-    services.ntp = {
+    services.ntp.enable = lib.mkForce false;
+    services.chrony = {
       enable = true;
-      servers = [ "pool.ntp.org" ];
+      servers = [
+        "0.de.pool.ntp.org"
+        "1.de.pool.ntp.org"
+        "2.de.pool.ntp.org"
+        "3.de.pool.ntp.org"
+      ];
+      extraConfig = ''
+        makestep 1.0 3
+        rtcsync
+      '';
     };
   };
 }
