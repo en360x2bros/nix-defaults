25cfbca604b6915ccd13dfcae6dc31540291ece7
Dennis Oderwald <oderwald@2bros-group.com>
2026-06-09 22:53:14 +0200

    DNS Improvements

--- defaults/dns.nix
+++ defaults/dns.nix
@@ -1,28 +1,44 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ lib, options, ... }:
 
+let
+  hasResolvedSettings = lib.hasAttrByPath [ "services" "resolved" "settings" "Resolve" ] options;
+in
 {
-  config = {
-    services.resolved = {
-      enable = true;
-      dnssec = "false"; # important to prevent dns issues
-      domains = [ "~." ];
-      fallbackDns = [
-        "8.8.4.4#eight.eight.four.four"
-        "1.0.0.1#one.zero.zero.one"
-        "2001:4860:4860::8844#eight.eight.four.four"
+  config = lib.mkMerge [
+    {
+      services.resolved.enable = true;
+
+      networking.nameservers = lib.mkDefault [
+        "1.1.1.1"
+        "9.9.9.9"
+        "2001:4860:4860::8888"
       ];
-      dnsovertls = "false"; # important to prevent dns issues
-    };
+    }
 
-    networking.nameservers = lib.mkDefault [
-      "1.1.1.1"
-      "9.9.9.9"
-      "2001:4860:4860::8888"
-    ];
-  };
+    (lib.mkIf hasResolvedSettings {
+      services.resolved.settings.Resolve = {
+        DNSSEC = "false"; # important to prevent dns issues
+        Domains = [ "~." ];
+        FallbackDNS = [
+          "8.8.4.4#eight.eight.four.four"
+          "1.0.0.1#one.zero.zero.one"
+          "2001:4860:4860::8844#eight.eight.four.four"
+        ];
+        DNSOverTLS = "false"; # important to prevent dns issues
+      };
+    })
+
+    (lib.mkIf (!hasResolvedSettings) {
+      services.resolved = {
+        dnssec = "false"; # important to prevent dns issues
+        domains = [ "~." ];
+        fallbackDns = [
+          "8.8.4.4#eight.eight.four.four"
+          "1.0.0.1#one.zero.zero.one"
+          "2001:4860:4860::8844#eight.eight.four.four"
+        ];
+        dnsovertls = "false"; # important to prevent dns issues
+      };
+    })
+  ];
 }
